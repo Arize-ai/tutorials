c2460ab716dd635464ba5a2326fb081c06145d8f
Arize AI Devs <support@arize.com>
2025-04-03 13:49:56 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: cd212807caed7913e5ea0434c1b3f3162d7690c9

---
+++ typescript/llm/tracing/beeai/README.md
@@ -0,0 +1,16 @@
+This is a tutorial for tracing LLM calls using BeeAI. You'll need to have Ollama installed along with a Phoenix API key. 
+
+[Signup for Phoenix Cloud here](https://app.phoenix.arize.com).
+[Install Ollama with llama 3.2 here](https://ollama.com).
+
+## Setup
+
+```bash
+yarn install
+```
+
+## Run
+
+```bash
+yarn start
+```
---
+++ typescript/llm/tracing/beeai/instrumentation.ts
@@ -0,0 +1,47 @@
+// instrumentation.ts
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+
+import { SEMRESATTRS_PROJECT_NAME } from "@arizeai/openinference-semantic-conventions";
+import { BeeAIInstrumentation } from "@arizeai/openinference-instrumentation-beeai";
+import * as beeaiFramework from "beeai-framework";
+
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR);
+
+const SERVICE_NAME = "beeai";
+// Initialize Instrumentation Manually
+const beeAIInstrumentation = new BeeAIInstrumentation();
+
+const provider = new NodeTracerProvider({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: SERVICE_NAME,
+    // defaults to "default" in the Phoenix UI
+    [SEMRESATTRS_PROJECT_NAME]: SERVICE_NAME,
+  }),
+  spanProcessors: [
+    // BatchSpanProcessor will flush spans in batches after some time,
+    // this is recommended in production. For development or testing purposes
+    // you may try SimpleSpanProcessor for instant span flushing to the Phoenix UI.
+    new BatchSpanProcessor(
+      new OTLPTraceExporter({
+        url: "https://app.phoenix.arize.com/v1/traces",
+        headers: { "api_key": process.env.PHOENIX_API_KEY || "" },
+      })
+    ),
+  ],
+});
+
+provider.register();
+
+
+console.log("🔧 Manually instrumenting BeeAgent...");
+beeAIInstrumentation.manuallyInstrument(beeaiFramework);
+console.log("✅ BeeAgent manually instrumented.");
+
+// eslint-disable-next-line no-console
+console.log("👀 OpenInference initialized");
